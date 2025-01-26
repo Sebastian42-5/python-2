b0bf57919f45b22571c6f8d64b13fd115b92abcd
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-01-26 10:50:05 -0500

    fixing code

--- lab1/mountain_streams.py
+++ lab1/mountain_streams.py
@@ -1,27 +1,37 @@
 def stream_mountain():
     
     initial_streams = int(input("Select a number of streams between 1 and 100: "))
-    initial_flows = [int(input("Select the flow for each stream")) for _ in range(initial_streams)]
-    streams = list(range(1, initial_streams - 1))
+    flows = [int(input(f"Select the flow for stream {i + 1}: ")) for i in range(initial_streams)]
 
     while True:
 
-        line = int(input("Do we 99 (split stream), 88 (merge stream) or 77? (reach the end of the mountain): "))
+        stream_action = int(input("Do we 99 (split stream), 88 (merge stream) or 77? (reach the end of the mountain): "))
 
-        if line == 77:
+        if stream_action == 77:
             break
-        elif line == 88:
-            number_stream = [int(input("Select the two streams to be merged together:")) for _ in range(2)]
-            streams.remove(number_stream[1])
-            initial_flows[number_stream[0]] += initial_flows[number_stream[1]]
-        elif line == 99:
-            number_stream, percent = int(input("Select the stream to be split: ")), int(input("Enter the percentage of each of the two new streams: ")) 
-            current_value = streams.pop(number_stream - 1)
-            streams.insert(number_stream - 1, current_value * percent / 100)
-            streams.insert(number_stream, current_value * (100 - percent) / 100)
+        elif stream_action == 88:
 
-    print(len(streams))
-    print(initial_flows[i] for i in range(len(initial_flows)))
+            first_stream = int(input("Select the first stream to be merged together: ")) - 1
+            second_stream = int(input("Select the second stream to be merged together: ")) - 1
+
+            flows[first_stream] += flows[second_stream]
+            flows.pop(second_stream)
+
+        elif stream_action == 99:
+
+            stream = int(input("Select the stream to be split: ")) - 1
+            percent = int(input("Enter the percentage of the first new stream: ")) 
+
+            original_flow = flows[stream]
+            first_new_flow = original_flow * percent / 100
+            second_new_flow = original_flow * (100 - percent) / 100
+            
+            flows[stream] = first_new_flow
+            flows.insert(stream + 1, second_new_flow)
+
+    print(len(flows))
+    for i, flow in enumerate(flows, start=1):
+        print(f"{flow:.2f}")
 
 stream_mountain()
 
