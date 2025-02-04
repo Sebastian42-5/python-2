6db06ffa4e4b89152e8237094eddaa31e121c61c
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-02-03 21:25:44 -0500

    some monday hw

--- week 3/primes.py
+++ week 3/primes.py
@@ -1,2 +1,31 @@
 # jupyterhub.dawsoncollege.qc.ca
+def isPrime(num):
+    # 1 is not a prime number
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        # if the number is a composite, returns false
+        if num % i == 0:
+            return False
+    return True
+
+number = int(input('Enter a number: '))
+number_list = list(range(2, number + 1))
+prime_numbers = [num for num in number_list if isPrime(num)]
+
+print(prime_numbers)
+
+
+
+
+    
+
+    
+        
+
+
+                
+
+
+
 
