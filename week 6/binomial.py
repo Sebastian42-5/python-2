c90e66f065c541b00164c13bb71a779f3a6d3206
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-02-26 11:09:53 -0500

    finishing class

--- week 6/binomial.py
+++ week 6/binomial.py
@@ -2,8 +2,43 @@ from math import factorial
 
 def binom(n, k):
     num = factorial(n) # this is O(n)
-    denom = factorial(k) * factorial(n - k) 
+    denom = factorial(k) * factorial(n - k) # this is 2 * O(n)
 
-    return num // denom 
+    return num // denom # this is O(n)
 
 print(binom(3, 2))
+
+
+def pascal_triangle(n):   # space complexity of n^2 (2 dimensional list)
+    matrix = []
+    for row in range(n): 
+        # This algorithm is O(n^3)
+        sub_lst = []
+        for k in range(row + 1):  
+            # this inner loop is n^2 
+            sub_lst.append(binom(row, k)) # if we can make this constant, we are good to go
+        matrix.append(sub_lst)
+
+    return matrix
+
+
+
+def print_pascal(matrix): 
+    for row in matrix: # our first loop is going O(n) times # a two dimensional list takes square space n^2
+        for i in range(len(row)): # worse case, we have n columns, so this is O(n) times as well 
+            print(row[i], end = '')
+        print()
+    
+
+print_pascal(pascal_triangle(5))
+
+
+# matrix[row][k]
+# matrix[row][k] = matrix[row - 1][k - 1] + matrix[row - 1][k]  4 primitive operations 
+
+
+def pascal_triangle_2(n):
+    pass
+
+
+# printing it all for each row you don't store it in a table, so the store space would be constant 
