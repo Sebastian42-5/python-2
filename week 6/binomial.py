ac3a945daa2058b9dd8dc3115f093d54ace801e2
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-02-26 18:11:58 -0500

    finishing hw

--- week 6/binomial.py
+++ week 6/binomial.py
@@ -26,7 +26,7 @@ def pascal_triangle(n):   # space complexity of n^2 (2 dimensional list)
 def print_pascal(matrix): 
     for row in matrix: # our first loop is going O(n) times # a two dimensional list takes square space n^2
         for i in range(len(row)): # worse case, we have n columns, so this is O(n) times as well 
-            print(row[i], end = '')
+            print(row[i], end = ''.center(4))
         print()
     
 
@@ -51,9 +51,40 @@ def pascal_triangle_2(n):
 
         matrix.append(sub_lst)
 
-    return ''.join(map()
+    return matrix
 
 
 # print_pascal(pascal_triangle_2(7))
 
 # printing it all for each row you don't store it in a table, so the store space would be constant 
+
+
+def pascal_triangle_3(n, current_row = 0, matrix = []):
+
+    if current_row == n:
+        return
+    if current_row == 0:
+
+        sub_lst = [1]
+
+    else:
+
+        sub_lst = [1]
+
+        last_row = matrix[-1]
+
+        for i in range(len(last_row) - 1):
+            sub_lst.append(last_row[i] + last_row[i + 1])
+        sub_lst.append(1)
+
+    matrix.append(sub_lst)
+
+    print('  '.join(map(str, sub_lst)).center(n * 4))
+
+    pascal_triangle_3(n, current_row + 1, matrix)
+
+
+
+# print_pascal(pascal_triangle_2(7)) 
+
+pascal_triangle_3(8)
