37543b1111f2837b698da807196e03c52b8cc032
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-02-27 09:28:34 -0500

    near the end of class

---
+++ week 6/butterflies_dict.py
@@ -0,0 +1,14 @@
+def new_sighting(butteflies, sighting):
+    pass
+
+
+butterflies = {'Monarch': 5, 'Painted Lady': 2, 'Bronze Copper': 12, 'Orange Sulphur': 7}
+
+for key in butterflies:
+    print(f'{key} : {butterflies[key]}')
+
+
+
+# kinds = ['Monarch', 'Painted Lady', 'Bronze Copper', 'Orange Sulphur']
+
+# counts = [5, 2, 12, 7]
--- week 6/dictionary_notes.py
+++ week 6/dictionary_notes.py
@@ -26,3 +26,35 @@ d = {'Monarch': 5, 'Common Blue': 12}
 d['Painted Lady'] = 3
 
 print(d)
+
+a = d.pop('Painted Lady')
+
+# print(a)
+
+# print(d)
+
+# del d['Monarch']
+
+# print(d)
+
+# keys in dictionaries are immutables
+# values can be mutable or immutable 
+
+# the keys have to be distinct
+
+list_keys = list(d.keys())
+
+print(list_keys)
+
+# .items() gives a tuple
+
+# pair in list(butterflies.times())
+
+
+d.update(Monarch = 12)
+
+print(d)
+
+d.update(Bronze = 1)
+
+print(d)
