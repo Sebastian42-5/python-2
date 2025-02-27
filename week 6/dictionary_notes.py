1faa478f7934f38b5d54429e88a8fb0e610ac6e6
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-02-27 09:16:40 -0500

    dictionaries

---
+++ week 6/dictionary_notes.py
@@ -0,0 +1,28 @@
+d = {}
+
+d = {'Monarch': 5, 'Common Blue': 12}
+
+# if 'starling' in d:
+#     print('in the dictionary')
+# else:
+#     print('not in the dictionary')
+
+# print(d['Monarch'])
+
+# print(d['Hello'])
+
+# print(d.get('starling'))
+
+# print(d.get('Monarch'))
+
+# print(len(d))
+
+# d2 = d.clear()
+
+# print(d2)
+
+d = {'Monarch': 5, 'Common Blue': 12}
+
+d['Painted Lady'] = 3
+
+print(d)
---
+++ week 6/parallel_butterflies.py
@@ -0,0 +1,29 @@
+def new_sighting(kinds, counts, sighting):
+    if sighting not in kinds:
+        kinds.append(sighting)
+        counts.append(0)
+
+    i = kinds.index(sighting)
+
+    counts[i] += 1
+    
+
+
+kinds = ['Monarch', 'Painted Lady', 'Bronze Copper', 'Orange Sulphur']
+
+counts = [5, 2, 12, 7]
+
+for i in range(len(kinds)):
+    print(f'{kinds[i]}: {counts[i]}')
+
+print()
+
+new_sighting(kinds, counts, 'Common Blue')
+
+for i in range(len(kinds)):
+    print(f'{kinds[i]}: {counts[i]}')
+
+print()
+
+
+
