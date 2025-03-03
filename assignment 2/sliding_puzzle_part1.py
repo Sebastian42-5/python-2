5696d0f5fc346eeea92efad897addf633cb1f364
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-03 11:29:44 -0500

    assignment 2

---
+++ assignment 2/sliding_puzzle_part1.py
@@ -0,0 +1,85 @@
+import random
+
+def tileLables(n):
+    str_lst = []
+    for i in range(1, n**2):
+        str_lst.append(str(i))
+    str_lst.append('  ')
+
+    return str_lst
+
+
+def getNewPuzzle(n):
+    puzzle = []
+    line = []
+    lines = tileLables(n)
+    random.shuffle(lines)
+    for _ in range(n):
+        for i in range(3):
+            line.append(lines[i])
+        puzzle.append(line)
+    puzzle = displayBoard(puzzle)
+    
+    return puzzle
+
+print(getNewPuzzle(3))
+
+
+def findEmptyTile(puzzle):
+    rows = len(puzzle)
+    columns = len(puzzle[0])
+
+    for i in range(rows):
+        for j in range(columns):
+            if puzzle[i][j] == '  ':
+                empty_coord = (i, j)
+    return empty_coord
+
+
+def nextMove(puzzle):
+    move = input('Enter your move: ')
+
+    if move == 'W':
+    
+    elif move == 'A':
+
+    elif move == 'S':
+    
+    elif move == 'D':
+
+    
+    
+
+
+
+
+
+# display board function provided
+
+def displayBoard(board_lst):
+    n = len(board_lst)
+
+    labels = []
+    for i in range(n):
+        for j in range(n):
+            labels.append(board_lst[i][j])
+
+    draw_board = ''
+    horizontal_div = ('+' + '------')*n + '+'
+    vertical_div = '|' + ' '*6
+    vertical_label = '|' + ' '*2 + '{}' + ' '*2
+    
+    for i in range(n):
+        draw_board = draw_board + horizontal_div +'\n'+\
+                    vertical_div*n + '|\n' + \
+                    vertical_label*n + '|\n'+\
+                    vertical_div*n + '|\n'
+    draw_board += horizontal_div
+    print(draw_board.format(*labels))
+        
+        
+
+
+
+
+
