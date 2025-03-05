9f94e681432a6765405042667610feaecb63a6cd
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-05 16:32:26 -0500

    starting practice question

--- week 7/practicequestions.py
+++ week 7/practicequestions.py
@@ -1,3 +1,21 @@
+
+# pandas + numpy + matplotlib
+
+# debugging and error messages
+
+# nested loops (two dimensional lists and complexity)
+
+# tuples
+
+# time space complexity 
+
+# dictionaries 
+
+# we need to come up with two mock questions and submit them by Friday 
+
+
+
+
 # topic 1 : combined 
 # question type and difficulty : long answer: medium
 
@@ -7,8 +25,35 @@
 
 # topic 1
 
-# topic 2
+'''
 
+
+'''
+
+
+
+
+# topic 2: Failed cooking book
+
+''' 
+You want to cook a matcha cheesecake for your mother. However, the cooking book has all of the steps completely reversed and numbered +2 instead of normally.
+
+Fix the cookbook (which is actually a dictionary where the keys are the steps and the values are the step numbers) so you can cook the cheesecake!
+
+'''
+
+# answer:
+
+def fixingCookbook(d: dict):
+
+    reversed_dict = dict(reversed(list(d.items())))
+
+    for key in reversed_dict:
+        reversed_dict[key] -= 2
+
+    return reversed_dict
+
+print(fixingCookbook({'bake': 5, 'cook': 4, 'water': 3}))
 
 
 
