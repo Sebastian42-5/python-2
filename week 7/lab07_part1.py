435fd032297f7ce731e9f20e494e9f300965bc5e
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-05 10:55:27 -0500

    in class

---
+++ week 7/lab07_part1.py
@@ -0,0 +1,32 @@
+'''
+given a dictionary of keys that are strings and/or integers, values are lists, write a snippet of code
+that returns total number of elements of all lists that have keys as strings 
+
+example :
+
+d  = {3: [3, 6], 'a': [3, 4, 5], 'b': [7, 8, 91, 1]}
+
+
+'''
+
+def dict_elements(dict):
+    pass
+
+
+
+'''
+write a function wordTally that takes an integer argument n and read n wors from the suer. Note that
+the user may enter the same word multiple times.
+
+Your function should tally up how many times each word occurs that the user has entred and store it in a dictionary where 
+the keys arfe the words and the values are the number of times each word occurs.
+
+return thsi dictionary.
+
+you may create only one collection, namely the dictionary to be returned.
+
+
+'''
+
+def wordTally(word):
+    pass
--- week 7/practicequestions.py
+++ week 7/practicequestions.py
@@ -1,3 +1,14 @@
-# topic 1 : 
+# topic 1 : combined 
+# question type and difficulty : long answer: medium
 
-# topic 2 : 
+# topic 2 : dictionaires
+# Question type and difficulty : Short answer: medium 
+
+
+# topic 1
+
+# topic 2
+
+
+
+
