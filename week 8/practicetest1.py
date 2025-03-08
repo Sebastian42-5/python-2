3ff07ec84f82e2200ce2008c72bc6577efa014d4
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-08 14:06:23 -0500

    done

--- week 8/practicetest1.py
+++ week 8/practicetest1.py
@@ -29,7 +29,52 @@
 
 # 10
 
+def countDigits(matrix, n):
+    
+    digit_count = 0
+
+    rows = len(matrix)
+
+    columns = len(matrix[0])
+
+    for i in range(rows):
+        for j in range(columns):
+            if int(matrix[i][j]) == n:
+                digit_count += 1
+    return digit_count
+
+
+# print(countDigits([['1', '1', '2'], ['3', '4', '1']], 1))
+
+
 # 11
 
 # 12
+
+# 13
+
+def count_frequences(lst: list):
+    freq = {}
+
+    for num in lst:
+        value = lst.count(num)
+        freq.update({num: value})
+    
+    return freq
+
+# print(count_frequences([1, 1, 2, 2, 2, 3, 3, 3, 3]))
+
+
+# 14
+
+def process_tuples(tup1, tup2): 
+    result = [] 
+    for i in range(len(tup1)): 
+        result.append(tup1[i] + tup2[i]) 
+    return tuple(result) 
+ 
+t1 = (1, 2, 3) 
+t2 = (4, 5, 6) 
+output = process_tuples(t1, t2) 
+print(output) 
 
