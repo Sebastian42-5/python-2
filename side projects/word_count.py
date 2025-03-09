56ee88e85c5da10592d06d2616fefb56ba2d6d1a
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-09 15:35:25 -0400

    random side project

---
+++ side projects/word_count.py
@@ -0,0 +1,10 @@
+paragraph = input('Enter your paragraph: ').split()
+
+characters = len(' '.join(paragraph))
+
+number_of_words = len(paragraph)
+
+print(f'Your paragraph has {number_of_words} words and {characters} characters!')
+
+
+
