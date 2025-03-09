8e919c2421943e2100c74e3aea4d3a65d9253ef9
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-09 15:50:13 -0400

    done

--- side projects/word_count.py
+++ side projects/word_count.py
@@ -1,10 +1,23 @@
+from collections import Counter 
+
 paragraph = input('Enter your paragraph: ').split()
 
-characters = len(' '.join(paragraph))
+characters = ' '.join(paragraph)
+
+number_of_characters = len(characters)
+
+number_of_sentences = characters.count('.') + characters.count('?') + characters.count('!')
 
 number_of_words = len(paragraph)
 
-print(f'Your paragraph has {number_of_words} words and {characters} characters!')
+most_common_words = Counter(paragraph)
+
+most_common_word = most_common_words.most_common()[0][0]
+
+print(f'Your paragraph has {number_of_words} words, {number_of_sentences} sentences, and {number_of_characters} characters! The most common word is : {most_common_word}')
+
+
+
 
 
 
