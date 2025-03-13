2b897753da420df05e053f38e5fad0b2bc5548be
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-13 08:04:45 -0400

    done

---
+++ week 8/homework.py
@@ -0,0 +1,30 @@
+'''
+Firgus is behind on several assignments. After
+rummaging through his backpack, he realizes that 
+he has N items, each of which he records as strings. 
+He has M upcoming assignments, the i-th of which requires 
+Ti items to complete, r1, r2, ... rTi. If he has
+all Ti required items, he can complete the i-th 
+assignment. Otherwise, he flunks it. How many asssignments
+can Firgus complete?
+
+INPUT SPECIFICATION
+
+--> first line contains 2 integers: N and M separated 
+by a space
+
+
+--> next N lines conatins a sunglke string si.
+it is guaranteed that all strings will be unique
+
+
+--> next M sections contain a single integer Ti, which is fol.owed by Ti lines each containing a single stringr ri .
+
+
+OUTPUT SPECIFICATION:
+
+
+
+
+
+'''
