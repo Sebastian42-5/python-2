8ae7f0724af2083485cc2f561eca3583bd2d4c82
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-04-01 20:36:19 -0400

    finishing hw

--- week 10/file_exceptions.py
+++ week 10/file_exceptions.py
@@ -14,34 +14,120 @@ else:
     for line in input_file:
         print(line.rstrip())
 
+
+
+
+
 input_file = open('sharks.txt', 'r', encoding = 'UTF8')
 
-shark_lst = input_file.readlines()
-output_file.writelines(shark_lst)
+shark_content = input_file.read()
+
+shark_lst = shark_content.split()
+
+word_count = len(shark_lst)
+
+# output_file.writelines(shark_lst)
+
+
+
+print(word_count)
+
+
 
 input_file.close()
-output_file.close()
+# output_file.close()
+
 
 
 '''
-1) Count the number of files 
+1) Count the number of words
+
+
 
 2) Make two files: cats.txt and dogs.txt. Store at least three names of cats in the first 
 file and three names of dogs in the second file. Write a program that tries to read these files
-and print the contents of the file to the screem. Wrap your code in a try except block to catch 
+and print the contents of the file to the screen. Wrap your code in a try except block to catch 
 the FileNotFoundError , and print a friendly message if a file is missing. Move one of the files to a different location on 
 your system, and make sure that the code in the except block executes proper
 
 
+
 3) modify your previous code to fail silently if either file is missing 
 
-4) One common problem when promting for numerical input occurs when providing 
+4) One common problem when prompting for numerical input occurs when providing 
 text instead of number. In such case, when trying to convert the input to int, a ValueError occurs. Write a program 
 that prompts the user for two numbers, add them together and print the result. Catch the ValueError if
 either input value is not a number, and print a friendly error message. Test your program by entering two 
 numbers and then by entering some text instead of a number.
 
 '''
+
+# 1)
+
+# 2) 
+
+dog_names = ['Albert ', 'Cupcake ', 'Winston ']
+
+cat_names = ['Destructor ', 'Terminator ', 'Ruler ']
+
+
+dog_file = open('dog.txt', 'w')
+cat_file = open('cat.txt', 'w')
+
+
+dog_file.writelines(dog_names)
+cat_file.writelines(cat_names)
+
+dog_file.close()
+cat_file.close()
+
+
+try:
+
+    dog_input = open('cats.txt', 'r')
+    cat_input = open('dog.txt', 'r')
+
+except FileNotFoundError:
+    print('One of the files does not exist.')
+
+else:
+    for line in dog_input:
+        print(line.rstrip())
+    for line in cat_input:
+        print(line.rstrip())
+
+
+    dog_input.close()
+    cat_input.close()
+
+
+
+# 3) 
+
+# I would change the print message under the except to a pass for my code to fail silently.
+
+# 4) 
+
+
+
+answer = input('You want to add to numbers or not? ')
+
+if answer == 'yes':
+
+    while True:
+        try:
+            num1 = int(input('Enter the first number to be added: '))
+            num2 = int(input('Enter the second number to be added: '))
+        
+        except ValueError:
+            print('One of your numbers is not an integer. Try again!')
+
+        else:
+            print(num1 + num2)
+            break
+
+else:
+    print('I guess next time...')    
 
 
 
