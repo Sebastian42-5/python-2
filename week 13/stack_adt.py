23183add9417dade4437d0e210e44f11472c963e
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-04-22 10:15:10 -0400

    starting the class

---
+++ week 13/stack_adt.py
@@ -0,0 +1,21 @@
+'''
+We are going to create our own abstract data structure
+
+S.push(5) Return value -- Stack contents 5
+
+S.push(3) Return value -- Stack content 5, 3
+
+S.isEmpty()
+
+S.top() Return Value 33 
+
+|  |
+|  |
+----
+
+This is how a stack looks like 
+
+
+'''
+
+
