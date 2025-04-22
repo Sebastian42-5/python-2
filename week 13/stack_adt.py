0709d9b0b8fdfff1102ccbe79147f68fa82a66e0
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-04-22 11:18:54 -0400

    done with class

--- week 13/stack_adt.py
+++ week 13/stack_adt.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from functools import total_ordering
+import time
+
 '''
 We are going to create our own abstract data structure
 
@@ -15,7 +19,171 @@ S.top() Return Value 33
 
 This is how a stack looks like 
 
+S.pop() 'Error' if it is empty 
+
+We need to define the error 
+
 
 '''
+class Empty(Exception):
+    '''error attempting to access an element from and empty container'''
+    pass
+
+
+@total_ordering
+class Stack: 
+
+    def __init__(self):
+        self.__data = []
+
+    
+    def __len__(self) -> int:
+        '''return length of stack'''
+        return len(self.__data)
+    
+    def isEmpty(self) -> bool:
+        return len(self.__data) == 0
+    
+    def push(self, elem: object) -> None:
+        '''add element elem to the top of the stack
+        
+        >>> s = Stack()
+        >>> s.push(5)
+        >>> s.push(3)
+        >>> s.pop()
+
+        3
+
+        '''
+        self.__data.append(elem)
+    
+    def top(self):
+        '''return (but do not remove) element at the top of stack'''
+
+        if self.isEmpty():
+            raise Empty('Stack is empty')
+
+        return self.__data[-1]
+    
+
+    def pop(self):
+        '''remove element at the top of stack and return its value'''
+
+        if self.isEmpty():
+            raise Empty('Stack is empty')
+
+        return self.__data.pop()
+    
+
+    def __repr__(self) -> str:
+
+        ESC = '\x1b'
+
+        YELLOW = ESC + '[33m'
+
+        BLUE = ESC + '[34m'
+
+        RESET = ESC + '[0m'
+
+        print(YELLOW + '|' + str(self.__data[-1]).center(9) + '|' + '\n' + '-' * 11)
+
+        for i in range(len(self.__data) - 1):
+
+            print(BLUE + '|' + str(self.__data[i]).center(9) + '|' + '\n' + '-' * 11 + RESET)
+
+
+        return ''
+
+
+    
+    def __lt__(self, other_stack: Stack) -> bool:
+        return isinstance(other_stack, Stack) and len(self.__data) < len(other_stack) 
+    
+    def __eq__(self, other_stack: Stack) -> bool:
+        
+        return isinstance(other_stack, Stack) and len(self.__data) == len(other_stack) 
+    
+    
+
+'''
+Base Exception 
+
+System Exit, KeyboardInterrupt, Exception
+
+Exception: (Empty), OsError, ArithmeticError, LookupError, NameError, ValueError, TypeError
+
+OsError: FileNotFoundError, PermissionError
+
+ArithmetcError: ZeroDivisionError
+
+LookupError: IndexError, KeyError
+
+'''
+
+
+
+    
+if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod(verbose = True)
+
+    # S = Stack()
+    # S.push(5)
+    # S.push(3)
+    # print(S)
+#     print(len(S))
+#     S.pop()
+#     print(S)
+#     print(S.isEmpty())
+#     S.pop()
+#     print(S)
+#     print(S.isEmpty())
+#     S.pop()
+#     print(S)
+
+
+    stack = Stack()
+    print(stack)
+    time.sleep(2)
+
+    stack.push(5)
+    stack.push(3)
+    print(stack)
+    time.sleep(2)
+
+    
+
+
+
+
+
+
+'''
+ESC = '\x1b'
+
+YELLOW = ESC + '[33m'
+
+BLUE = ESC + '[34m'
+
+GREEN = ESC + '[32m'
+
+RED = ESC + '[31m'
+
+RESET = ESC + '[0m'
+
+
+'''
+    
+
+
+
+
+
+
+# in the main program, with no __, the user can access the data. We want it to be private
+
+# One underscore is not advised, so it's half accessible
+
+# with def len() is s.len and def __len__() is len(s)
 
 
