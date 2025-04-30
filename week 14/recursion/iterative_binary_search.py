215eac1d086e8f531751fb15a9b6224565ae43fe
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-04-30 11:16:37 -0400

    finishing class

--- week 14/recursion/iterative_binary_search.py
+++ week 14/recursion/iterative_binary_search.py
@@ -3,6 +3,9 @@
 
 # we assume that the list is sorted.
 
+# we can visualize the time complexity with a decision tree
+
+
 def binarySearch(lst, target):
 
     nums = lst
@@ -39,3 +42,56 @@ if result != 1:
     print(f'Element {num} is present at index {result}')
 else:
     print(f'Element {num} was not found in the list')
+
+
+'''
+< > 
+
+8 -> 2^0
+4 12 -> 2^1
+2 6 10 14 -> 2^2
+1 3 5 7 9 11 13 15 -> 2^3
+
+Four comparisons
+
+Worse case you start from the root and you get to the tree
+
+If you find the height of the three, it's the run time 
+
+The height of the tree is O(log(n))
+
+n = 2^0 + 2^1 + 2^2 + ... + 2^h = 2^h+1 - 1
+
+n = 2^h+1 - 1
+
+n + 1 = 2^h + 1
+
+log(n + 1) = lg(2^h + 1)
+
+log(n + 1) = lg(2) (h + 1)
+
+h = log(n + 1) - 1
+
+
+log(n + 1) - 1 belongs to O(lgn) 
+
+c n0 for all n >= n0 s.t
+
+lg(n + 1) <= clgn + 1
+
+n + 1 <= n^2
+
+lg(n + 1) <= lg(n^2) + 1
+
+= 2lg(n) + 1
+
+let c = 2 and n0 = 2
+
+proof done
+
+
+Every node has two children at most 
+
+This is a complete binary tree 
+
+'''
---
+++ week 14/recursion/recursive_binary_search.py
@@ -0,0 +1,38 @@
+def binarySearch(lst, low, high, target):
+    if high >= low:
+        mid = (high + low) // 2
+
+        if lst[mid] == target:
+            return mid
+        
+        elif lst[mid] > target: # search in left half
+            return binarySearch(lst, low, mid - 1, target)
+        
+        else:
+            return binarySearch(lst, mid + 1, high, target)
+    
+    else:
+        return - 1
+    
+
+lst = [3, 5, 7, 8, 9, 10, 12, 14, 17, 19]
+
+target = 17
+
+low = 0
+
+high = len(lst) - 1
+
+result = binarySearch(lst, low, high, target)
+
+
+
+if result != 1:
+    print(f'Element {target} is present at index {result}')
+else:
+    print(f'Element {target} was not found in the list')
+
+        
+    
+
+
