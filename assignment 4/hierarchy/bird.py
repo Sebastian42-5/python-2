5677a1495008ef8b8aec064b7aec335d90a0c385
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-05-07 15:16:17 -0400

    done

--- assignment 4/hierarchy/bird.py
+++ assignment 4/hierarchy/bird.py
@@ -4,6 +4,7 @@ class Bird(Animal):
 
     def __init__(self, wings = 2, legs = 2):
         self.wings = wings
+        super().__init__(legs)
 
     def __repr__(self) -> str:
         return super().__repr__() + '\nClass: Bird\n'
