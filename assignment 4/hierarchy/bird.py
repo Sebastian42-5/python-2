635db45fd3aa330a5219d6c21bd53b5f267ef8ba
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-05-07 11:18:03 -0400

    done

--- assignment 4/hierarchy/bird.py
+++ assignment 4/hierarchy/bird.py
@@ -2,7 +2,7 @@ from animal import Animal
 
 class Bird(Animal):
 
-    def __init__(self, wings = 2):
+    def __init__(self, wings = 2, legs = 2):
         self.wings = wings
 
     def __repr__(self) -> str:
